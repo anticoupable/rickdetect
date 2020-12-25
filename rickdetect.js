c05649b50fcd52f2c9c8b8108c76ed6e9ec2f498
Johan <41506568+anticoupable@users.noreply.github.com>
2020-12-25 18:02:24 +0100

    Update rickdetect.js

--- rickdetect.js
+++ rickdetect.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch'); // https://www.npmjs.com/package/node-fetch
 var urlExpander = require('expand-url'); // https://www.npmjs.com/package/expand-url
 
 
-term("Veuillez insérer un lien ici : ") // Demande de texte
+term("Veuillez insérer un lien ici : "); // Demande de texte
 term.inputField(function(error, input){
 
     // Obtenir le lien original si il est raccourci (Ne fonctionne pas dans certains cas (Genre dans les liens Grabify))
@@ -15,17 +15,17 @@ term.inputField(function(error, input){
         .then(body => {
             // Regarder si le code de la page contient "never gonna give you up", "rick roll" ou "rick astley"
             if(body.toLowerCase().includes("never gonna give you up") || body.toLowerCase().includes("rick roll") || body.toLowerCase().includes("rick astley")){
-                term.red("\nIl se peut que votre lien contienne \"Never Gonna Give You Up\".") // Indiquer que le lien est un rick roll
+                term.red("\nIl se peut que votre lien contienne \"Never Gonna Give You Up\"."); // Indiquer que le lien est un rick roll
             } else {
-                term.green("\nRien de suspect n'a été détecté.") // Indiquer si rien de suspect n'a été détecté
+                term.green("\nRien de suspect n'a été détecté."); // Indiquer si rien de suspect n'a été détecté
             }
-            process.exit() // Arrêter le processus
+            process.exit(); // Arrêter le processus
         })
         .catch(err => {
             // Si une erreur s'est produite, Afficher un message d'erreur et arrêter le processus
-            term.red("\nUne erreur s'est produite, Veuillez entrer un lien valide (Il doit commencer par http / https et exister).")
-            process.exit()
-        })
+            term.red("\nUne erreur s'est produite, Veuillez entrer un lien valide (Il doit commencer par http / https et exister).");
+            process.exit();
+        });
     });
     }
 );
